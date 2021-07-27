45526e9ad8bd0478b9f9d888d7f62246846795ef
IsmailAtaev <ataewisma@gmail.com>
2021-07-27 20:32:44 +0300

    To-Improve-Queue

--- src/Queue/Queue.java
+++ src/Queue/Queue.java
@@ -1,61 +1,64 @@
 package Queue;
 
+import java.util.Arrays;
+import java.util.Objects;
 
-
-
-public class Queue {
-    private int length;
-    private int capacity = 10;
-    private Integer queue[];
-   // private Object qq[];
+public class Queue<T> {
+    private Integer length = 0;
+    private Integer capacity = 10;
+    private T queue[];
 
     public Queue() {
-       // this.queue =  new Object[capacity];
-     //   this.qq = new E[capacity];
+        // this.queue =  new Object[capacity];
+        //   this.qq = new E[capacity];
         //this.queue = new E[capacity];
         //this.queue = new E[capacity];
-
-
+        this.queue = (T[]) new Object[this.getCapacity()];
+        //* if i ismailskom write this.queue=  new Object[this.capasite];
+        //* auto Casting prohodit  jdk autoCast do
+        //* i do not Know  maybe(может) it is standart
     }
 
-    public Queue(int capacity) {
-        if (capacity > 0) {
+    public Queue(Integer capacity) {
+        /*if (capacity > 0) {
             this.capacity = capacity;
            // this.queue = new Integer[this.capacity];
-            this.queue = new Integer [capacity];
+            //this.queue = new Integer [capacity];
+            this.queue = new Object[];
         } else {
             if (capacity != 0) {
                 throw new IllegalArgumentException(" Capacity menshe 0 zero" + capacity);
             }
             //this.queue = new Integer[this.capacity];
             this.queue = new Integer[this.capacity];
-        }
+        }*/
+        this.capacity = capacity;
+        this.queue = (T[]) new Object[this.getCapacity()];
     }
 
-
-    public boolean addQueue(Integer value) {
-        if (this.capacity > 0 && this.length < this.capacity) {
-
-            this.queue[this.length] = value;
-
+    public boolean add(T value) {
+        // changed this.capacity to, this.getCapacity
+        if (this.getCapacity() > 0 && this.getLength() < this.getCapacity()) {
+            this.queue[this.getLength()] = value;
             this.length++;
             return true;
-        } else if (this.length == this.capacity) {
+        } else if (this.getLength() == this.getCapacity()) {
             this.capacity *= 2;
-            Integer temp[] = new Integer[capacity];
-            for (int i = 0; i < this.length; i++) {
+            T temp[] = (T[]) new Object[capacity];
+            for (int i = 0; i < this.getLength(); i++) {
                 temp[i] = this.queue[i];
             }
-            temp[this.length] = value;
+            temp[this.getLength()] = value;
             this.queue = temp;
             return true;
-        } else
-            return false;
+        }
+        return false;
     }
 
     public boolean remove() {
-        if (!this.isEmpty()) {
-            Integer[] temp = new Integer[this.length];
+        /*if (!this.isEmpty()) {
+            T temp[] = (T[]) new Object[this.getCapacity()];
+            // Integer[] temp = new Integer[this.length];
             for (int i = 0; i < this.length - 1; i++) {
                 temp[i] = this.queue[i + 1];
             }
@@ -64,17 +67,22 @@ public class Queue {
             //this.capacity -= this.length + 2; //
             this.capacity = this.length + 2; //
             return true;
-        } else
-            return false;
+        }*/
+        if (!this.isEmpty()) {
+            for (int i = 0; i < this.getLength() - 1; i++) {
+                this.queue[i] = this.queue[i + 1];
+            }
+            this.queue[this.length--] = null;
+            this.capacity = this.length + 2;
+            return true;
+        }
+        return false;
     }
 
-    public void printQueue() {
-        if (!this.isEmpty()) {
-            for (int i = 0; i < this.length; i++) {
-                System.out.println(this.queue[i]);
-            }
-        } else
-            System.out.println("Do not element to print ");
+    public void print() {
+        for (int i = 0; i < this.getLength(); i++) {
+            System.out.print(this.queue[i] + " ");
+        }
     }
 
     public boolean isEmpty() {
@@ -89,58 +97,47 @@ public class Queue {
         return this.capacity;
     }
 
-    public Integer get(Integer index) {
+    public T get(Integer index) {
         try {
-            if (this.isEmpty() || index > this.length || index < 0) {
-                throw new Exception(" size error index > or < 0 ");
+            --index;
+            if (this.isEmpty() || index > this.getLength() || index < 0) {
+                throw new Exception("Size error index > or < 0 ");
             }
-            return this.queue[index - 1];
-
+            return this.queue[index];
         } catch (Exception ex) {
             System.out.println(ex.getMessage());
         }
         return null;
     }
 
-    public void clearQ() {
+    public void clear() {
         this.capacity = 10;
         this.length = 0;
-        this.queue = new Integer[this.capacity];
+        this.queue = (T[]) new Integer[this.getCapacity()];
     }
 
-    public boolean contains(Integer elem) { // polucaem object
-        if (!this.isEmpty()) {
-            for (int i = 0; i < this.length; i++) {
-                if (this.queue[i] == elem) {
-                    return true;
-                }
+    public boolean contains(T value) {
+        for (int i = 0; i < this.getLength(); i++) {
+            //if (this.queue[i] == element) { // used equals method()
+            if (this.queue[i].equals(value)) {
+                return true;
             }
         }
         return false;
     }
 
-    public Integer indexObjet(Integer data) { // znacene return Object
-        try {
-            if (this.isEmpty()) {
-                throw new IndexOutOfBoundsException(" Method indexObject index error ");
+    public Integer indexObjet(T data) {
+        for (int i = 0; i < this.getLength(); i++) {
+            if (this.queue[i].equals(data)) {
+                return i + 1;
             }
-            for (int i = 0; i < this.length; i++) {
-                if (this.queue[i] == data) {
-                    return i + 1;
-                }
-            }
-        } catch (IndexOutOfBoundsException ix) {
-            System.out.println(ix.getMessage());
         }
-        /**
-         * try has fynall and fynalize i do not know  из-зи этого не использовал
-         * */
-        return null; // it is bad  Возврашать ноль zero very very bad (I thing Ismailskom :) )
+        return null; // it is bad  Возврашать ноль zero very very bad (I thing Ismailskom)
     }
 
-    public boolean set(Integer data) {
+    public boolean set(T data) {
         if (!this.isEmpty()) {
-            this.addQueue(data);
+            this.add(data);
             return true;
         }
         return false;
@@ -148,31 +145,26 @@ public class Queue {
 
     // что то я не то зделала tommorow finished
     // ешо не понял как typmlate typename зделать шаблонный
-    /*public Queue[] subQueue(int from, int to) {
+   /* public Queue<T> subQueue(Integer from, Integer to) {
         if (!this.isEmpty()) {
             try {
-                if (to > this.length || to < 0 || from > to || from < 0) {
-                    throw new IndexOutOfBoundsException(" from to size big or small sorry it is erorr ");
+                if (to > this.getLength() || to < 0 || from > to || from < 0) {
+                    throw new IndexOutOfBoundsException("From to size big or small sorry it is erorr ");
                 }
-                int count = to - from;
-                Integer[] temp = new Integer[count];
-                Queue qq[] = new  Queue[count];
-                for (int i = 0; i < count; i++, from++) {
-                    temp[i] = this.queue[from];
-                    qq.
-
+                Integer count = to - from;
+                Queue<T> qq = new Queue<T>(count--);
+                while (from < to) {
+                    qq.add(this.get(from));
                 }
                 return qq;
             } catch (IndexOutOfBoundsException ex) {
                 System.out.println(ex.getMessage());
             }
         }
-        Integer v[] = new Integer[0];
-        v[0] = null;
-        return v;
+        return null;
     }*/
-    public boolean addQueue(Queue data) {
-        if (!data.isEmpty()) {
+    public boolean addQueue(Queue<T> data) {
+       /* if (!data.isEmpty()) {
             Integer count = this.length + data.length;
             Integer elem[] = new Integer[count + 2];
             int i = 0;
@@ -191,58 +183,103 @@ public class Queue {
             this.queue = elem;
             return true;
         }
+        return false;*/
+
+        if (!data.isEmpty()) {
+            Integer overallSize = this.getLength() + data.getLength();
+            T element[] = (T[]) new Object[overallSize];
+            int i = 0;
+            int j = 0;
+            while (i < this.getLength()) {
+                element[i] = this.get(i + 1);
+                ++i;
+            }
+            while (i < overallSize) {
+                element[i] = data.get(j + 1);
+                i++;
+                j++;
+            }
+            this.length = overallSize;
+            this.capacity = overallSize + 2;
+            this.queue = element;
+            return true;
+        }
         return false;
     }
-}
 
-class qMain{
-    public static void main(String[] args) {
-        Queue queue =  new Queue();
-        for (int i = 0 ; i < 12; i++){
-            queue.addQueue(i * 10);
-        }
-        queue.printQueue();
-        System.out.println(queue.isEmpty());
-        System.out.println(queue.getLength());
-        System.out.println(queue.getCapacity()+ "\n\n");
-        queue.remove();
-        queue.printQueue();
-        System.out.println("\n\n");
-        queue.remove();
-        queue.printQueue();
-        System.out.println("\n\n");
-        queue.remove();
-        queue.printQueue();
-        System.out.println(queue.get(10));
-        System.out.println("\n\n\n");
-        System.out.println(" has it queue object sent " + queue.contains(30));
-        System.out.println(" has it queue object sent " + queue.contains(33));
-        System.out.println(" method IndexObject  = " + queue.indexObjet(50) );
-        System.out.println(" method IndexObject  = " + queue.indexObjet(55) );
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Queue<?> queue1 = (Queue<?>) o;
+        return Objects.equals(length, queue1.length) && Objects.equals(capacity, queue1.capacity) && Arrays.equals(queue, queue1.queue);
+    }
 
-        // Queue ff[] = queue.subQueue(3,6);
-
-        Queue queue1 = new Queue();
-        for (int j =0; j < 5; j++){
-            queue1.addQueue(j*2);
-        }
-        queue1.printQueue();
-        System.out.println("\n\n\n");
-        queue.addQueue(queue1);
-        queue.printQueue();
-
-        queue.clearQ();
-        queue.printQueue();
-
+    @Override
+    public int hashCode() {
+        int result = Objects.hash(length, capacity);
+        result = 31 * result + Arrays.hashCode(queue);
+        return result;
     }
 }
 
+class qMain {
+    public static void main(String[] args) {
+        Queue<Integer> queue = new Queue<Integer>();
 
-class DD<E>{
-    private DD elem[];
+        for (int i = 0; i < 12; i++) {
+            queue.add(i * 10);
+        }
+        queue.print();
 
-    public DD(int capacity){
-        elem = new DD[capacity];
+        System.out.print("\n");
+        System.out.println(" Queue isEmpty = " + queue.isEmpty());
+        System.out.println(" Queue Lenght = " + queue.getLength());
+        System.out.println(" Queue Capacity = " + queue.getCapacity());
 
+
+        queue.remove();
+        queue.print();
+        System.out.println("\n\n");
+
+        queue.remove();
+        queue.print();
+        System.out.println("\n\n");
+
+        queue.remove();
+        queue.print();
+        System.out.print("\n\n");
+
+        System.out.print(queue.get(10));
+        System.out.print("\n\n");
+
+        System.out.println(".get(index) =  " + queue.get(5));
+        System.out.println();
+
+        System.out.print("Has it queue object sent " + queue.contains(30) + "\n\n");
+        System.out.print("Has it queue object sent " + queue.contains(33) + "\n\n");
+        System.out.print("Method IndexObject(50)  = " + queue.indexObjet(50) + "\n\n");
+        System.out.print("Method IndexObject(55)  = " + queue.indexObjet(55) + "\n\n");
+
+        Queue queue1 = new Queue();
+        for (int j = 0; j < 5; j++) {
+            queue1.add(j * 2);
+        }
+
+        System.out.print("queue1.print() = ");
+        queue1.print();
+        System.out.print("\n\n");
+
+        System.out.print("queue1.print() = ");
+        queue.addQueue(queue1);
+        queue.print();
+        System.out.print("\n\n");
+
+       /* Queue<Integer> temp = queue.subQueue(3, 7);
+        System.out.println("subQueue = ");
+        temp.print();*/
+
+        queue.clear();
+        queue.print();
     }
 }
